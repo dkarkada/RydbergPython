e61408b28c4b61a2b16317d7477669efd2d51541
dkarkada <dkarkada@gmail.com>
2017-09-04 23:55:30 -0500

    need that green dot
    sorta did sorta node stuff sorta

--- lattice_data.py
+++ lattice_data.py
@@ -14,14 +14,14 @@ class LatticeData:
     def create_lattice(self, size):
         self.lattice = np.zeros((size, size), dtype=np.int8)
         self.flip_on(0, 0)
-    
+
     def generate_changes(self, p, g):
         print(self.lattice)
-        
+
     def flip_on(self, x, y):
         rc = self.get_rc(x, y)
         self.lattice[rc[0], rc[1]] = 1
-    
+
     def get_rc(self, x, y):
         if self.lattice is not None:
             sz = self.lattice.shape[0]
@@ -35,4 +35,46 @@ class LatticeData:
             return sz//2 - r, c - sz//2
         else:
             return None
+
+
+class CoordinateNode:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def set_next(self, node):
+        self.next = node
+
+    def set_prev(self, node):
+        self.prev = node
+
+    def get_next(self):
+        return self.next
+
+    def get_prev(self):
+        return self.prev
+
+
+class CoordinateLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def add_node(self, node):
+        if self.head is None:
+            self.head = node
+            self.tail = node
+        else:
+            self.tail.set_next(node)
+            node.set_prev(self.tail)
+            self.tail = node
+
+    def remove_node(self, node):
+        if node is self.head:
+            self.head = node.get_next()
+            self.head.set_prev(None)
+            #random shit idk
+        el:
+            
+        del node
         
