ee2c5af40f616cf8ffcb8a307fb519e83a29aa7a
dkarkada <dkarkada@gmail.com>
2017-09-05 23:51:45 -0500

    Finished linkedlist implementation
    instead of cancerous parallel-link system, i just copied linkedlist into regular list. Although it costs O(n), it allows for traversal with modification (traverse list copy, modify linkedlist) in a straightforward and noncancerous way

--- lattice_data.py
+++ lattice_data.py
@@ -36,11 +36,20 @@ class LatticeData:
         else:
             return None
 
+"""
+Implementation of nodes for a LinkedList which represent a coordinate
+position on the lattice. LinkedList implementation useful for fast O(1)
+removal. to_list function is a necessary O(n) cost in order to traverse
+and remove (e.g. convert to list, traverse list, remove from linkedlist)
+"""
+
 
 class CoordinateNode:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        self.next = None
+        self.prev = None
 
     def set_next(self, node):
         self.next = node
@@ -60,7 +69,7 @@ class CoordinateLinkedList:
         self.head = None
         self.tail = None
 
-    def add_node(self, node):
+    def add(self, node):
         if self.head is None:
             self.head = node
             self.tail = node
@@ -69,12 +78,25 @@ class CoordinateLinkedList:
             node.set_prev(self.tail)
             self.tail = node
 
-    def remove_node(self, node):
-        if node is self.head:
+    def remove(self, node):
+        if node is self.head and node is self.tail:
+            self.head = None
+            self.tail = None
+        elif node is self.head:
             self.head = node.get_next()
             self.head.set_prev(None)
-            #random shit idk
-        el:
-            
+        elif node is self.tail:
+            self.tail = node.get_prev()
+            self.tail.set_next(None)
+        else:
+            node.get_prev.set_next(node.get_next())
+            node.get_next.set_prev(node.get_prev())
         del node
-        
+
+    def to_list(self):
+        result = []
+        node = self.head
+        while node is not None:
+            result.append(node)
+            node = node.get_next()
+        return result
